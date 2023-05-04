85ac3c862acb4e2025932bbca440b9d039eb0c19
Aaron Borgi <aaronborgi1@gmail.com>
2023-05-04 02:26:58 +0100

    finished how to apply screen, simple video tweaks remaining

--- lib/screens/how_to_apply.dart
+++ lib/screens/how_to_apply.dart
@@ -1,28 +1,49 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class HowToApplyScreen extends StatelessWidget {
+import '../widgets/nails/custom_app_bar.dart';
+
+class HowToApplyScreen extends StatefulWidget {
   const HowToApplyScreen({super.key});
+
+  @override
+  State<HowToApplyScreen> createState() => _HowToApplyScreenState();
+}
+
+class _HowToApplyScreenState extends State<HowToApplyScreen> {
+  late VideoPlayerController videoPlayerController;
+  late ChewieController? _chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+    initializePlayer();
+  }
+
+  Future<void> initializePlayer() async {
+    videoPlayerController =
+        VideoPlayerController.asset("assets/nails/nail_demo_video.mp4");
+    await Future.wait([videoPlayerController.initialize()]);
+    _createChewieController();
+  }
+
+  void _createChewieController() {
+    _chewieController =
+        ChewieController(videoPlayerController: videoPlayerController);
+  }
+
+  @override
+  void dispose() {
+    videoPlayerController.dispose();
+    _chewieController?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Container(
-          margin: EdgeInsets.only(
-            top: MediaQuery.of(context).size.height * 0.06,
-          ),
-          child: Image.asset(
-            "assets/EssentialAppBar.png",
-            fit: BoxFit.cover,
-            height: 120,
-          ),
-        ),
-        flexibleSpace: Image.asset(
-          "assets/AppBarBackground.png",
-          fit: BoxFit.cover,
-          height: double.infinity,
-        ),
-      ),
+      appBar: const CustomAppBar(),
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
@@ -35,7 +56,7 @@ class HowToApplyScreen extends StatelessWidget {
             Align(
               alignment: Alignment.topLeft,
               child: Container(
-                margin: const EdgeInsets.only(top: 10, bottom: 20),
+                margin: const EdgeInsets.only(top: 20, bottom: 20),
                 width: MediaQuery.of(context).size.width * 0.6,
                 height: 69,
                 decoration: const BoxDecoration(
@@ -78,6 +99,95 @@ class HowToApplyScreen extends StatelessWidget {
                   fontSize: 25,
                   fontFamily: "Gotham",
                   fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+            AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Expanded(
+                child: Center(
+                  child: _chewieController != null &&
+                          _chewieController!
+                              .videoPlayerController.value.isInitialized
+                      ? Chewie(
+                          controller: _chewieController!,
+                        )
+                      : Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            CircularProgressIndicator(),
+                            SizedBox(height: 20),
+                            Text('Loading'),
+                          ],
+                        ),
+                ),
+              ),
+            ),
+            Center(
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 20),
+                padding: const EdgeInsets.all(20),
+                width: MediaQuery.of(context).size.width * 0.6,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Colors.white),
+                  gradient: const RadialGradient(
+                    radius: 3,
+                    colors: [
+                      Color.fromRGBO(165, 239, 255, 0.2),
+                      Color.fromRGBO(110, 191, 244, 0.04),
+                      Color.fromRGBO(70, 144, 212, 0),
+                    ],
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        Icon(
+                          Icons.circle,
+                          color: Color.fromRGBO(255, 255, 255, 0.5),
+                          size: 8,
+                        ),
+                        Icon(
+                          Icons.circle,
+                          color: Color.fromRGBO(255, 255, 255, 0.5),
+                          size: 8,
+                        ),
+                        Icon(
+                          Icons.circle,
+                          color: Color.fromRGBO(255, 255, 255, 0.5),
+                          size: 8,
+                        ),
+                        SizedBox(width: 5),
+                        Text(
+                          "Color Gel Polish",
+                          style: TextStyle(
+                            shadows: [
+                              Shadow(
+                                color: Color.fromRGBO(0, 0, 0, 0.25),
+                                offset: Offset(0, 3.5),
+                              ),
+                            ],
+                            color: Colors.white,
+                            fontFamily: "Gotham",
+                            fontSize: 15,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Text(
+                      "Time of polymerization in light of the UV lamp-2-3minutes LED-lamp-1 minute",
+                      style: TextStyle(
+                        fontFamily: "Gotham",
+                        fontWeight: FontWeight.w400,
+                        fontSize: 12,
+                        color: Color.fromRGBO(235, 235, 235, 1),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
--- lib/widgets/nails/custom_app_bar.dart
+++ lib/widgets/nails/custom_app_bar.dart
@@ -1,6 +1,3 @@
-
-
-
 import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
@@ -15,26 +12,20 @@ class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
       centerTitle: false,
       actions: [
         Container(
-            margin: EdgeInsets.only(
-              right: MediaQuery.of(context).size.height * 0.04,
-            ),
-            child: Image.asset("assets/PlayButton.png")),
+          margin: EdgeInsets.only(
+            right: MediaQuery.of(context).size.height * 0.04,
+          ),
+          child: Image.asset("assets/PlayButton.png"),
+        ),
       ],
       title: Container(
         margin: EdgeInsets.only(
           top: MediaQuery.of(context).size.height * 0.08,
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset(
-              "assets/EssentialAppBar.png",
-              fit: BoxFit.cover,
-              height: 120,
-            ),
-            Image.asset("assets/PlayButton.png")
-          ],
+        child: Image.asset(
+          "assets/EssentialAppBar.png",
+          fit: BoxFit.cover,
+          height: 120,
         ),
       ),
       flexibleSpace: Image.asset(
@@ -44,8 +35,7 @@ class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
       ),
     );
   }
-  
+
   @override
-  // TODO: implement preferredSize
-  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
