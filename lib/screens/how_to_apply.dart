e04f2fc4c7c72e6e3c16da90615c01a4de370ed7
Aaron Borgi <aaronborgi1@gmail.com>
2023-05-04 01:13:04 +0100

    how to apply page

--- lib/screens/how_to_apply.dart
+++ lib/screens/how_to_apply.dart
@@ -30,6 +30,59 @@ class HowToApplyScreen extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                margin: const EdgeInsets.only(top: 10, bottom: 20),
+                width: MediaQuery.of(context).size.width * 0.6,
+                height: 69,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(0),
+                    topRight: Radius.circular(75),
+                    bottomLeft: Radius.circular(0),
+                    bottomRight: Radius.circular(75),
+                  ),
+                  image: DecorationImage(
+                    image: AssetImage("assets/AppBarBackground.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Text(
+                        "How to apply",
+                        style: TextStyle(
+                          fontFamily: "Gotham",
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Image.asset("assets/PlayButton.png"),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const Center(
+              child: Text(
+                "Nails Polish",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontFamily: "Gotham",
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
