b4a54e54547374bb2d139de5484d0d0fe0758256
Aaron Borgi <aaronborgi1@gmail.com>
2023-05-06 00:39:01 +0100

    beginning of development of flasch screen

--- lib/screens/flasch_nail.dart
+++ lib/screens/flasch_nail.dart
@@ -18,7 +18,7 @@ class FlaschNailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton.large(
         backgroundColor: Colors.transparent,
         elevation: 0,
@@ -31,13 +31,65 @@ class FlaschNailScreen extends StatelessWidget {
       ),
       appBar: const CustomAppBar(),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Get.back();
-          },
-          child: Row(
-            children: [for (var nail in flaschNailImages) Image.asset(nail)],
-          ),
+        child: Column(
+          children: [
+            const Text(
+              "Flasch Nail",
+              style: TextStyle(
+                fontFamily: "Gotham",
+                fontWeight: FontWeight.w700,
+                color: Color.fromRGBO(35, 40, 55, 1),
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 30),
+            Container(
+              child: Column(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width -
+                        MediaQuery.of(context).size.width / 8,
+                    child: Row(
+                      children: [
+                        for (var nail in flaschNailImages)
+                          Image.asset(
+                            nail,
+                            width: MediaQuery.of(context).size.width / 8,
+                          ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.5,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image:
+                            AssetImage("assets/flasch_nail/FlaschNailBg.png"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Center(
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width -
+                            MediaQuery.of(context).size.width / 8,
+                        child: Row(
+                          children: [
+                            for (var nail in flaschNailImages)
+                              Image.asset(
+                                nail,
+                                width: MediaQuery.of(context).size.width / 8,
+                              ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
