7dc6a3fbf2c9299a20e41d3270d980b2118cc3c3
hamza <hamza.sghaier@axam.tn>
2023-05-06 11:24:55 +0100

    Add screen util and device previeww packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:essential_beauty/screens/home.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import 'screens/splash.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  await ScreenUtil.ensureScreenSize();
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => const MyApp(), // Wrap your app
+  ));
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,20 +23,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(1920, 1080),
-         minTextAdapt: true,
+      minTextAdapt: true,
       splitScreenMode: true,
       builder: (BuildContext context, Widget? child) {
+        // ScreenUtil.init(context);
         return GetMaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Essential Beauty',
+          useInheritedMediaQuery: true,
+          builder: DevicePreview.appBuilder,
           theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
           ),
           home: child,
         );
-       },
-       child: const Home(),
+      },
+      child: const Home(),
     );
   }
 }
@@ -38,6 +49,7 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //  ScreenUtil.init(context);
     return AnimatedSplashScreen(
       splashIconSize: MediaQuery.of(context).size.height,
       splash: const Splash(),
