121dd32a32c8f7ea4441eb9b7aa1b37825d4975d
Aaron Borgi <aaronborgi1@gmail.com>
2023-05-06 17:28:26 +0100

    fixed how to apply screen for tablet

--- lib/screens/how_to_apply.dart
+++ lib/screens/how_to_apply.dart
@@ -55,148 +55,192 @@ class _HowToApplyScreenState extends State<HowToApplyScreen> {
         ),
         child: Row(
           children: [
-            Align(
-              alignment: Alignment.topLeft,
-              child: Container(
-                margin: const EdgeInsets.only(top: 20, bottom: 20),
-                width: 536.w,
-                height: 147.h,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(0),
-                    topRight: Radius.circular(75),
-                    bottomLeft: Radius.circular(0),
-                    bottomRight: Radius.circular(75),
-                  ),
-                  image: DecorationImage(
-                    image: AssetImage("assets/AppBarBackground.png"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                child: GestureDetector(
-                  onTap: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(
-                        "How to apply",
-                        style: TextStyle(
-                          fontFamily: "Gotham",
-                          fontWeight: FontWeight.w700,
-                          fontSize: 35.sp,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Image.asset("assets/PlayButtonLarge.png"),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Center(
-              child: Text(
-                "Nails Polish".toUpperCase(),
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 25,
-                  fontFamily: "Gotham",
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-            ),
-            const SizedBox(height: 25),
-            AspectRatio(
-              aspectRatio: 16 / 9,
-              child: Expanded(
-                child: Center(
-                  child: _chewieController != null &&
-                          _chewieController!
-                              .videoPlayerController.value.isInitialized
-                      ? Chewie(
-                          controller: _chewieController!,
-                        )
-                      : Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [
-                            CircularProgressIndicator(),
-                            SizedBox(height: 20),
-                            Text('Loading'),
-                          ],
-                        ),
-                ),
-              ),
-            ),
-            Center(
-              child: Container(
-                margin: const EdgeInsets.symmetric(vertical: 20),
-                padding: const EdgeInsets.all(20),
-                width: MediaQuery.of(context).size.width * 0.6,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(color: Colors.white),
-                  gradient: const RadialGradient(
-                    radius: 3,
-                    colors: [
-                      Color.fromRGBO(165, 239, 255, 0.2),
-                      Color.fromRGBO(110, 191, 244, 0.04),
-                      Color.fromRGBO(70, 144, 212, 0),
-                    ],
-                  ),
-                ),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: const [
-                        Icon(
-                          Icons.circle,
-                          color: Color.fromRGBO(255, 255, 255, 0.5),
-                          size: 8,
-                        ),
-                        Icon(
-                          Icons.circle,
-                          color: Color.fromRGBO(255, 255, 255, 0.5),
-                          size: 8,
-                        ),
-                        Icon(
-                          Icons.circle,
-                          color: Color.fromRGBO(255, 255, 255, 0.5),
-                          size: 8,
-                        ),
-                        SizedBox(width: 5),
-                        Text(
-                          "Color Gel Polish",
-                          style: TextStyle(
-                            shadows: [
-                              Shadow(
-                                color: Color.fromRGBO(0, 0, 0, 0.25),
-                                offset: Offset(0, 3.5),
-                              ),
-                            ],
-                            color: Colors.white,
-                            fontFamily: "Gotham",
-                            fontSize: 15,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const Text(
-                      "Time of polymerization in light of the UV lamp-2-3minutes LED-lamp-1 minute",
-                      style: TextStyle(
-                        fontFamily: "Gotham",
-                        fontWeight: FontWeight.w400,
-                        fontSize: 12,
-                        color: Color.fromRGBO(235, 235, 235, 1),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            const LeftSide(),
+            RightSide(chewieController: _chewieController),
           ],
         ),
       ),
+    );
+  }
+}
+
+class RightSide extends StatelessWidget {
+  const RightSide({
+    super.key,
+    required ChewieController? chewieController,
+  }) : _chewieController = chewieController;
+
+  final ChewieController? _chewieController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Center(
+          child: Container(
+            margin: EdgeInsets.symmetric(vertical: 80.h),
+            child: Text(
+              "Nails Polish".toUpperCase(),
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontFamily: "Gotham",
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 25),
+        SizedBox(
+          width: 927.93.w,
+          height: 600.h,
+          child: AspectRatio(
+            aspectRatio: 16 / 9,
+            child: Expanded(
+              child: Center(
+                child: _chewieController != null &&
+                        _chewieController!
+                            .videoPlayerController.value.isInitialized
+                    ? Chewie(
+                        controller: _chewieController!,
+                      )
+                    : Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          CircularProgressIndicator(),
+                          SizedBox(height: 20),
+                          Text('Loading'),
+                        ],
+                      ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class LeftSide extends StatelessWidget {
+  const LeftSide({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.topLeft,
+          child: Container(
+            margin: const EdgeInsets.only(top: 20, bottom: 20),
+            width: 536.w,
+            height: 147.h,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(0),
+                topRight: Radius.circular(75),
+                bottomLeft: Radius.circular(0),
+                bottomRight: Radius.circular(75),
+              ),
+              image: DecorationImage(
+                image: AssetImage("assets/AppBarBackground.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: GestureDetector(
+              onTap: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "How to apply",
+                    style: TextStyle(
+                      fontFamily: "Gotham",
+                      fontWeight: FontWeight.w700,
+                      fontSize: 35.sp,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Image.asset("assets/PlayButtonLarge.png"),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 20),
+          padding: const EdgeInsets.all(40),
+          width: 460.87.w,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: Colors.white),
+            gradient: const RadialGradient(
+              radius: 3,
+              colors: [
+                Color.fromRGBO(165, 239, 255, 0.2),
+                Color.fromRGBO(110, 191, 244, 0.04),
+                Color.fromRGBO(70, 144, 212, 0),
+              ],
+            ),
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    children: const [
+                      Icon(
+                        Icons.circle,
+                        color: Color.fromRGBO(255, 255, 255, 0.5),
+                        size: 8,
+                      ),
+                      SizedBox(width: 5),
+                      Icon(
+                        Icons.circle,
+                        color: Color.fromRGBO(255, 255, 255, 0.5),
+                        size: 8,
+                      ),
+                      SizedBox(width: 5),
+                      Icon(
+                        Icons.circle,
+                        color: Color.fromRGBO(255, 255, 255, 0.5),
+                        size: 8,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 40),
+                  const Text(
+                    "Color Gel Polish",
+                    style: TextStyle(
+                      shadows: [
+                        Shadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.25),
+                          offset: Offset(0, 3.5),
+                        ),
+                      ],
+                      color: Colors.white,
+                      fontFamily: "Gotham",
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ],
+              ),
+              const Text(
+                "Time of polymerization in light of the UV lamp-2-3minutes LED-lamp-1 minute",
+                style: TextStyle(
+                  fontFamily: "Gotham",
+                  fontWeight: FontWeight.w400,
+                  fontSize: 24,
+                  color: Color.fromRGBO(235, 235, 235, 1),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
